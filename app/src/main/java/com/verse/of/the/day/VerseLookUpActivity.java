143d84d0022a264eeb15839b5ec2e4aa9fb91c68
Josh <reimer.josh.2006@gmail.com>
2024-08-20 21:04:50 -0300

    enhanced verse lookup feature

--- app/src/main/java/com/verse/of/the/day/VerseLookUpActivity.java
+++ app/src/main/java/com/verse/of/the/day/VerseLookUpActivity.java
@@ -1,17 +1,20 @@
 package com.verse.of.the.day;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.content.Context;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.TypedValue;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 import java.util.ArrayList;
+import java.util.Objects;
+
 public class VerseLookUpActivity extends AppCompatActivity{
 
-	TextView scrolled_text;
-	LinearLayout ll;
+	LinearLayout linear_layout;
 	Verse verse_for_lookup;
 
 	@Override
@@ -20,32 +23,54 @@ public class VerseLookUpActivity extends AppCompatActivity{
 		setContentView(R.layout.verse_lookup_activity);
 		Bible bible = new Bible();
 		Tools tools = new Tools();
-		ll = findViewById(R.id.ll);
+		linear_layout = findViewById(R.id.ll);
 
-		ArrayList<Verse> verses = new ArrayList<Verse>();
-		ArrayList<TextView> tvs;
+		TextView pre_verse_textview = new TextView(this);
+		TextView verse_textview = new TextView(this);
+		TextView post_verse_textview = new TextView(this);
 
-		
-		TextView scrolled_text = new TextView(this);
-		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-				LinearLayout.LayoutParams.MATCH_PARENT,
-				LinearLayout.LayoutParams.MATCH_PARENT
-		);
+		linear_layout.addView(pre_verse_textview);
+		linear_layout.addView(verse_textview);
+		linear_layout.addView(post_verse_textview);
 
-		//scrolled_text.setId(0);
-		scrolled_text.setLayoutParams(params);
-		ll.addView(scrolled_text);
+		pre_verse_textview.setTextSize(20f);
+
+		verse_textview.setTextSize(21f);
+		verse_textview.setTypeface(verse_textview.getTypeface(), Typeface.BOLD);
+		TypedValue typedValue = new TypedValue();
+		getTheme().resolveAttribute(android.R.attr.colorPrimary, typedValue, true);
+		int color = typedValue.data;
+		verse_textview.setTextColor(color);
+
+		post_verse_textview.setTextSize(20f);
+
+		String pre_verse_textview_text = "";
+		String verse_textview_text = "";
+		String post_verse_textview_text = "";
+
 		String intentExtras = getIntent().getStringExtra("verse_ref");
 
-		verse_for_lookup = getVerseFromMainActivity(this,intentExtras);
+        assert intentExtras != null;
+        verse_for_lookup = getVerseFromMainActivity(this,intentExtras);
 
 		String chapter = bible.getChapter(this,tools,verse_for_lookup.book, verse_for_lookup.chapter);
 		String[] str_verses = chapter.split("\n");
+
 		for(int i = 0; i < str_verses.length; i++){
-			Verse c = new Verse(this,verse_for_lookup.book_int,str_verses[i]);
-			verses.add(c);
+			Verse current_verse = new Verse(this,verse_for_lookup.book_int,str_verses[i]);
+			if (current_verse.verse < verse_for_lookup.verse){
+				pre_verse_textview_text += "\n"+str_verses[i];
+			}
+			if(Objects.equals(verse_for_lookup.reference, current_verse.reference)) {
+				verse_textview_text += str_verses[i];
+			}
+			if (current_verse.verse > verse_for_lookup.verse){
+				post_verse_textview_text += str_verses[i]+"\n";
+			}
 		}
-		scrolled_text.setText(chapter);
+		pre_verse_textview.setText(pre_verse_textview_text);
+		verse_textview.setText(verse_textview_text);
+		post_verse_textview.setText(post_verse_textview_text);
 	}
 
 	Verse getVerseFromMainActivity(Context c,String intent_extras){
