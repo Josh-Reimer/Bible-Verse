bad2aaddcc3edd90b344bfeebf0cebbf3f4ba5e8
Josh <reimer.josh.2006@gmail.com>
2024-09-05 20:29:08 -0300

    added basic bookmark functionality

---
+++ app/src/main/java/com/verse/of/the/day/bookmark_dao.java
@@ -0,0 +1,17 @@
+package com.verse.of.the.day;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface bookmark_dao {
+    @Insert
+    void insertAll(bookmark bookmark);
+
+    @Query("SELECT * FROM bookmarks")
+    List<bookmark> getAllBookmarks();
+
+}
