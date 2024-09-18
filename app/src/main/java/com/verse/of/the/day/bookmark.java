a3a1f7f310350ae1f996590c0c308bcb2acf0a64
Josh <reimer.josh.2006@gmail.com>
2024-09-18 20:33:51 -0300

    fixed duplicate bookmark issue

--- app/src/main/java/com/verse/of/the/day/bookmark.java
+++ app/src/main/java/com/verse/of/the/day/bookmark.java
@@ -2,12 +2,18 @@ package com.verse.of.the.day;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "bookmarks")
+@Entity(tableName = "bookmarks",
+        indices = {@Index(value = {"bible_reference"}, unique = true)})
 public class bookmark {
     @PrimaryKey(autoGenerate = true)
-    public int id;
+    public int item_id;
+
+    @ColumnInfo
+    public String bible_reference;
+
     @ColumnInfo(name = "bookmark")
     public String bookmark;
 
--- app/src/main/java/com/verse/of/the/day/bookmark_dao.java
+++ app/src/main/java/com/verse/of/the/day/bookmark_dao.java
@@ -2,16 +2,16 @@ package com.verse.of.the.day;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.List;
 
 @Dao
 public interface bookmark_dao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insertAll(bookmark bookmark);
 
     @Query("SELECT * FROM bookmarks")
     List<bookmark> getAllBookmarks();
-
 }
