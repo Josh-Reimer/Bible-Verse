513a16ce8996fce1872cf0423a57145769d2af32
Josh <reimer.josh.2006@gmail.com>
2024-10-05 23:38:27 -0300

    bookmarks can be deleted now
    bookmarks can be deleted on the apps home page by tapping the bookmark icon again

--- app/src/main/java/com/verse/of/the/day/bookmark_dao.java
+++ app/src/main/java/com/verse/of/the/day/bookmark_dao.java
@@ -1,6 +1,7 @@
 package com.verse.of.the.day;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
@@ -17,4 +18,8 @@ public interface bookmark_dao {
 
     @Query("SELECT * FROM bookmarks WHERE bible_reference==:bible_reference")
     List<bookmark> getBookmark(String bible_reference);
+
+    @Query("DELETE FROM bookmarks WHERE bible_reference = :bible_reference")
+    void deleteBookmark(String bible_reference);
+
 }
