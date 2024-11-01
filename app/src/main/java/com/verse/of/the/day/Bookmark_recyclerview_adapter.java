b1b85110cfd6faaeb4794b9d4b016ee348f1628a
Josh <reimer.josh.2006@gmail.com>
2024-10-31 22:20:58 -0300

    bookmarks can be opened in verselookup
    bookmarks can now be opened in the verselookup screen from the bookmarks screen.

--- app/src/main/java/com/verse/of/the/day/Bookmark_recyclerview_adapter.java
+++ app/src/main/java/com/verse/of/the/day/Bookmark_recyclerview_adapter.java
@@ -1,5 +1,9 @@
 package com.verse.of.the.day;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,19 +11,17 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
 public class Bookmark_recyclerview_adapter extends
         RecyclerView.Adapter<Bookmark_recyclerview_adapter.ViewHolder>{
 
-    ArrayList<Bookmark_recyclerview_model> data = new ArrayList<>();
+    static ArrayList<Bookmark_recyclerview_model> data = new ArrayList<>();
+
     @NonNull
     @Override
     public Bookmark_recyclerview_adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.bookmark_recyclerview_item,null));
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.bookmark_recyclerview_item,parent,false));
     }
 
     @Override
@@ -33,13 +35,24 @@ public class Bookmark_recyclerview_adapter extends
         return data.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder{
         TextView verse;
         TextView book;
         public ViewHolder(@NonNull View itemView){
             super(itemView);
             verse = itemView.findViewById(R.id.text_view_priority);
             book = itemView.findViewById(R.id.bookview);
+
+            // Define click listener for the ViewHolder's View.
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Navigate to VerseLookup activity with verse details
+                    Intent intent = new Intent(itemView.getContext(), VerseLookUpActivity.class);
+                    intent.putExtra("verse_ref", data.get(getAdapterPosition()).ref);
+                    startActivity(itemView.getContext(),intent,null);
+                }
+            });
     }
     }
     public Bookmark_recyclerview_adapter(ArrayList<Bookmark_recyclerview_model> data) {
--- app/src/main/java/com/verse/of/the/day/Bookmark_recyclerview_model.java
+++ app/src/main/java/com/verse/of/the/day/Bookmark_recyclerview_model.java
@@ -3,8 +3,10 @@ package com.verse.of.the.day;
 public class Bookmark_recyclerview_model {
     String scripture_text;
     String book;
-    public Bookmark_recyclerview_model(String scripture_text, String book){
+    String ref;
+    public Bookmark_recyclerview_model(String scripture_text, String book, String ref){
         this.scripture_text = scripture_text;
         this.book = book;
+        this.ref = ref;
     }
 }
